c7502eee4b5e7e8bd3c506419fd1ef09cbe6b474
Jewell Vormawor <jewellvormawor@gmail.com>
2024-09-27 22:54:54 -0400

    updated dunder init with static method

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -9,3 +9,21 @@ def multiply(a,b):
 
 def divide(a,b):
     return a / b
+
+class Calculator:
+    @staticmethod
+    def add(a,b):
+        calculation = Calculation(a, b, add)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def subtract(a,b):
+        calculation = Calculation(a, b, subtract)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def multiply (a,b):
+        calculation = Calculation(a, b, multiply)  # Pass the add function from calculator.operations
+        return calculation.get_result()
+    @staticmethod
+    def divide(a,b):
+        calculation = Calculation(a, b, divide)  # Pass the add function from calculator.operations
+        return calculation.get_result()
