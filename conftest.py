4525c7af41882d27ca4f4ce673b68fe354514959
Jewell Vormawor <jewellvormawor@gmail.com>
2024-10-02 22:18:28 -0400

    moved to be under tests

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest  # Import pytest module
-
-def pytest_addoption(parser):
-    parser.addoption("--num_records", action="store", default="10", help="Number of records")
-
-@pytest.fixture
-def num_records(request):
-    return int(request.config.getoption("--num_records"))
