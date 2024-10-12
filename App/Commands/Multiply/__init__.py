e8bd0e3d4cf159375f327dee1cc4aa578b020cd9
Jewell Vormawor <jewellvormawor@gmail.com>
2024-10-11 22:42:24 -0400

    update multiply command

--- App/Commands/Multiply/__init__.py
+++ App/Commands/Multiply/__init__.py
@@ -2,11 +2,10 @@ import sys
 from App.Commands import Command
 
 
-class MultiplyCommand(Command):
+class MultiplyCommand:
     def __init__(self, a: float, b: float):
         self.a = a
         self.b = b
 
-    def execute(self):
-        result = self.a * self.b
-        print(f"The result of multiplying {self.a} and {self.b} is {result}")
+    def execute(self) -> float:
+        return self.a * self.b
