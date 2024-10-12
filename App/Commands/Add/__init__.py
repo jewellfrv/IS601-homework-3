f186bc28b89f6a555ddc844e6bd2f5032e344523
Jewell Vormawor <jewellvormawor@gmail.com>
2024-10-11 22:44:50 -0400

    update add command

--- App/Commands/Add/__init__.py
+++ App/Commands/Add/__init__.py
@@ -2,10 +2,10 @@ import sys
 from App.Commands import Command
 
 class AddCommand(Command):
-    def __init__(self, a: float, b: float):
+    def __init__(self, a, b):
         self.a = a
         self.b = b
 
     def execute(self):
         result = self.a + self.b
-        return self.a + self.b
+        print(result)
